af9fdf87667ff6d7b0b2e888c24ca6a367277ea2
Ednalyn C. De Dios <ednalyn.dedios@gmail.com>
2021-01-28 22:23:30 -0600

    change git commands

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,19 +4,19 @@ function prompt {"Dd $(get-location)>"}
 Set-Alias -Name jn -Value jupyter-notebook
 
 function Get-GitInit { & git init $args }
-New-Alias -Name ginit -Value Get-GitInit
+New-Alias -Name gii -Value Get-GitInit
 
 function Get-GitStatus { & git status $args }
-New-Alias -Name gs -Value Get-GitStatus
+New-Alias -Name gss -Value Get-GitStatus
 
 function Set-GitAdd { & git add . $args }
-New-Alias -Name gadd -Value Set-GitAdd
+New-Alias -Name gaa -Value Set-GitAdd
 
 function Set-GitCommit { & git commit -m $args }
-New-Alias -Name gcom -Value Set-GitCommit
+New-Alias -Name gcc -Value Set-GitCommit
 
 function Set-GitRemoteAddOriginMain { & git remote add origin $args }
-New-Alias -Name grao -Value Set-GitRemoteAddOriginMain
+New-Alias -Name grr -Value Set-GitRemoteAddOriginMain
 
 function Set-GitPush { & git push -u origin main $args }
 New-Alias -Name gpush -Value Set-GitPush
